1b039f4fcf61510f5e7f80594910ea00828ca974
Vanessa <colorfulwaves@gmail.com>
2023-08-24 13:29:26 -0400

    adding weight saves and value saves into main file

---
+++ main.py
@@ -0,0 +1,252 @@
+
+#%% 
+import time
+import matplotlib.pyplot as plt
+import torch
+import torch.optim as optim
+from torch_geometric.loader import DataLoader
+from src.device import device_info
+from src.data import TempDataset
+from src.model import GCN_Temp 
+import pandas as pd
+from src.process import train, validation, predict
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from scipy.stats import pearsonr
+
+device_information = device_info()
+print(device_information)
+device = device_information.device
+
+start_time = time.time()
+
+## SET UP DATALOADERS: ---
+
+# Build starting dataset: 
+# dataset = TempDataset('C:\\Users\\color\\Documents\\Bilodeau_Research_Python\\Critical_Temp_Research\\critical_temp_GCNN\\csv_data\\No_outliers_smile_dataset.csv')
+dataset = TempDataset('/home/jbd3qn/Downloads/critical_temp_GCNN/csv_data/No_outliers_smile_dataset.csv')
+print('Number of NODES features: ', dataset.num_features)
+print('Number of EDGES features: ', dataset.num_edge_features)
+
+finish_time_preprocessing = time.time()
+time_preprocessing = (finish_time_preprocessing - finish_time_preprocessing) / 60
+
+# # Number of datapoints in the training set:
+training_test_percentage = 0.8
+n_train = int(len(dataset) * training_test_percentage)
+
+# # Number of datapoints in the validation set:
+n_val = len(dataset) - n_train
+
+# # Define pytorch training and validation set objects:
+train_set, val_set = torch.utils.data.random_split(
+    dataset, [n_train, n_val], generator=torch.Generator().manual_seed(42) #TODO 
+)
+
+# # Build pytorch training and validation set dataloaders:
+batch_size = 10
+dataloader = DataLoader(dataset, batch_size, shuffle=True)
+
+
+train_dataloader = DataLoader(train_set, batch_size, shuffle=True)
+val_dataloader = DataLoader(val_set, batch_size, shuffle=True)
+
+
+
+## RUN TRAINING LOOP: ---
+
+# Train with a random seed to initialize weights:
+torch.manual_seed(0)
+
+# Set up model:
+
+initial_dim_gcn = dataset.num_features
+edge_dim_feature = dataset.num_edge_features
+
+model =  GCN_Temp(initial_dim_gcn, edge_dim_feature).to(device)
+
+# Set up optimizer:
+learning_rate = 1e-3
+optimizer = optim.Adam(model.parameters(), learning_rate)
+
+train_losses = []
+val_losses = []
+
+best_val_loss = float('inf')  # infinite
+
+start_time_training = time.time()
+num_of_epochs = 200
+for epoch in range(1, num_of_epochs): #TODO
+
+    train_loss = train(model, device, train_dataloader, optimizer, epoch)
+    train_losses.append(train_loss)
+
+    val_loss = validation(model, device, val_dataloader, epoch)
+    val_losses.append(val_loss)
+    
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        
+        torch.save(model.state_dict(), "best_model_weights.pth")
+
+finish_time_training = time.time()
+time_training = finish_time_training -start_time_training
+print("\n -Training finished: {:3f} seconds".format(time))
+
+#Testing:
+weights_file = "best_model_weights.pth"
+
+# Training:
+input_all, target_all, pred_prob_all = predict(model, train_dataloader, device)
+
+
+r2_train = r2_score(target_all.cpu(), pred_prob_all.cpu())
+mae_train = mean_absolute_error(target_all.cpu(), pred_prob_all.cpu())
+rmse_train = mean_squared_error(target_all.cpu(), pred_prob_all.cpu(), squared=False)
+r_train, _ = pearsonr(target_all.cpu(), pred_prob_all.cpu())
+
+# Validation:
+
+input_all, target_all, pred_prob_all = predict(model, val_dataloader, device, weights_file)
+
+r2_val = r2_score(target_all.cpu(), pred_prob_all.cpu())
+mae_val = mean_absolute_error(target_all.cpu(), pred_prob_all.cpu())
+rmse_val = mean_squared_error(target_all.cpu(), pred_prob_all.cpu(), squared=False)
+r_val, _ = pearsonr(target_all.cpu(), pred_prob_all.cpu())
+
+
+# plots
+
+# training vs validation losses
+plt.plot(train_losses, label='Train losses')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Train losses')
+
+plt.plot(val_losses, label='Validation losses')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Losses')
+plt.show()
+
+
+
+# training stats
+legend_text = "R2 Score: {:.4f}\nR Pearson: {:.4f}\nMAE: {:.4f}\nRMSE: {:.4f}".format(
+    r2_train, r_train , mae_train, rmse_train
+)
+
+plt.figure(figsize=(4, 4), dpi=100)
+plt.scatter(target_all.cpu(), pred_prob_all.cpu(), alpha=0.3)
+plt.plot([min(target_all.cpu()), max(target_all.cpu())], [min(target_all.cpu()),
+                                                            max(target_all.cpu())], color="k", ls="--")
+plt.xlim([min(target_all.cpu()), max(target_all.cpu())])
+plt.title('Training')
+plt.xlabel("True Values")
+plt.ylabel("Predicted Values")
+plt.legend([legend_text], loc="lower right")
+plt.show()
+
+
+# Validation stats
+legend_text = "R2 Score: {:.4f}\nR Pearson: {:.4f}\nMAE: {:.4f}\nRMSE: {:.4f}".format(
+    r2_val, r_val , mae_val, rmse_val
+)
+
+plt.figure(figsize=(4, 4), dpi=100)
+plt.scatter(target_all.cpu(), pred_prob_all.cpu(), alpha=0.3)
+plt.plot([min(target_all.cpu()), max(target_all.cpu())], [min(target_all.cpu()),
+        max(target_all.cpu())], color="k", ls="--")
+plt.xlim([min(target_all.cpu()), max(target_all.cpu())])
+plt.title('Validation')
+plt.xlabel("True Values")
+plt.ylabel("Predicted Values")
+plt.legend([legend_text], loc="lower right")
+plt.show()
+
+
+print('hola mundo')
+
+
+# %%
+
+
+#Times
+finish_time = time.time()
+time_prediction = (finish_time - finish_time_training) / 60
+total_time = (finish_time - start_time) / 60
+print("\n //// Preprocessing time: {:3f} minutes ////".format(time_preprocessing))
+print("\n //// Training time: {:3f} minutes ////".format(time_training))
+print("\n //// Prediction time: {:3f} minutes ////".format(time_prediction))
+print("\n //// Total time: {:3f} minutes ////".format(total_time))
+#results DataFrame
+
+data = {
+    "Metric": [
+        "number_features",
+        "num_edge_features",
+        "initial_dim_gcn ",
+        "edge_dim_feature",
+        "hidden_dim_nn_1 ",
+        "p1 ",
+        "hidden_dim_nn_2 ",
+        "p2 ",
+        "hidden_dim_nn_3 ",
+        "p3 ",
+        "hidden_dim_fcn_1 ",
+        "hidden_dim_fcn_2 ",
+        "hidden_dim_fcn_3 ",
+        "training_test_percentage %",
+        "batch_size", 
+        "learning_rate",
+        "number_of_epochs",
+        "r2_train",
+        "r_train",
+        "mae_train",
+        "rmse_train", 
+        "r2_validation",
+        "r_validation",
+        "mae_validation",
+        "rmse_validation",
+        "time_preprocessing", 
+        "time_training",
+        "time_prediction",
+        "total_time"
+    ],
+    "Value": [
+        dataset.num_features,
+        dataset.num_edge_features,
+        initial_dim_gcn,
+        edge_dim_feature ,
+        # hidden_dim_nn_1 ,
+        # p1 ,
+        # hidden_dim_nn_2 ,
+        # p2 ,
+        # hidden_dim_nn_3,
+        # p3 ,
+        # hidden_dim_fcn_1 ,
+        # hidden_dim_fcn_2 ,
+        # hidden_dim_fcn_3 ,
+        training_test_percentage*100,
+        batch_size,
+        learning_rate,
+        num_of_epochs,
+        r2_train, 
+        r_train, 
+        mae_train, 
+        rmse_train,
+        r2_val,
+        r_val,
+        mae_val, 
+        rmse_val,
+        time_preprocessing, 
+        time_training,
+        time_prediction,
+        total_time
+    ],
+    
+}
+
+df = pd.DataFrame(data)
+df.to_csv('results/results.csv', index=False)
+
+# %%
