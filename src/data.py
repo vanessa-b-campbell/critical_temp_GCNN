4f3cef9d407dac868c3f673bc9b86f35fcb4414c
Vanessa <colorfulwaves@gmail.com>
2023-09-07 12:06:44 -0400

    attempt to fix dataset class

--- src/data.py
+++ src/data.py
@@ -12,13 +12,23 @@ class TempDataset(InMemoryDataset):
     # next time- review the InMemoryDataset tutorial and figure out how to not have 
     # raw_name be defaulted in the constructor, or how to override
     # the default when calling the constructor in T_V_main.py
-    def __init__(self, root='/home/jbd3qn/Downloads/critical_temp_GCNN/chemprop_splits_csv',raw_name ,processed_name ,transform=None, pre_transform=None, log=True):
+    def __init__(self, root='/home/jbd3qn/Downloads/critical_temp_GCNN/chemprop_splits_csv',transform=None, pre_transform=None, pre_filter=None, log=True):
         
-        self.filename = os.path.join(root,raw_name)
-        self.processed_filename = os.path.join(root,processed_name)
+        # gives TempDataset class the attributes from the InMemoryDataset class (inheritance)
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+    
+    @property
+    def  raw_file_names(self):
+         return ['train_full.csv', 'val_full', 'test_full']
+        # self.filename = os.path.join(root,raw_name)
+        # self.processed_filename = os.path.join(root,processed_name)
         
-        # read a csv from that path:
-        self.df = pd.read_csv(self.filename)
+        # # read a csv from that path:
+        # self.df = pd.read_csv(self.filename)
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
         
         # assign dataset attribute "input_vectors" to be the 2048 bit vector representing each molecule:
         self.x = self.df[self.df.columns[0]].values
@@ -27,9 +37,9 @@ class TempDataset(InMemoryDataset):
         self.y = self.df[self.df.columns[-1]].values   
         
         
-        super(TempDataset, self).__init__(root, transform, pre_transform)
-        ##################################################################################    what does this do
-        self.data, self.slices = torch.load(self.processed_paths[0])
+        # super(TempDataset, self).__init__(root, transform, pre_transform)
+        # ##################################################################################    what does this do
+        # self.data, self.slices = torch.load(self.processed_paths[0])
         
 
     def processed_file_names(self):
